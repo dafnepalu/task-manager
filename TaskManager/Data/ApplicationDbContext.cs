949a070a6264e6b4b332f713aad69ea6f91d45fc
Dafne Palú <dafne.palu@ebankit.com>
2023-12-04 12:34:01 +0000

    Created the UserTask entity and a new Migration to introduce it into the DB

--- TaskManager/Data/ApplicationDbContext.cs
+++ TaskManager/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TaskManager.Entities;
 
 namespace TaskManager.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public DbSet<UserTask> UserTasks {get;set;}
     }
 }
---
+++ TaskManager/Entities/UserTask.cs
@@ -0,0 +1,17 @@
+﻿namespace TaskManager.Entities
+{
+    public class UserTask
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required bool IsCompleted { get; set; }
+        public required Priority Priority { get; set; }
+    }
+
+    public enum Priority
+    {
+        High,
+        Medium,
+        Low
+    }
+}
